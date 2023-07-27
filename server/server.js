eb69f737e8249c66793c7b110c7c956b22db7192
James.gao <james.gao@grays.com.au>
2023-07-27 17:49:35 +1000

    BE init

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+
+app.get("/api",(req, res)=>{
+    res.json({ "users": ["user1", "user2", "user3"]})
+})
+
+
+app.listen( 3000, ()=>{ console.log("started on port 3000")})

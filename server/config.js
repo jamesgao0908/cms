e85529b67d7ba390d776bbe1f2b8e55e486f7c8e
James.gao <james.gao@grays.com.au>
2024-01-27 10:12:44 +1100

    update

--- server/config.js
+++ server/config.js
@@ -5,7 +5,7 @@ dotenv.config();
 const config = {
   port: 8080,
   database: {
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST,
     username: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || 'password',
     dbName: process.env.DB_DATABASE || 'CMS',

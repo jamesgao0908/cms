07af7ac452ef5bc6d37628542abc21510f0b275f
James.gao <james.gao@grays.com.au>
2024-01-28 23:24:39 +1100

    update

--- client/src/services/config/api_getHeader.js
+++ client/src/services/config/api_getHeader.js
@@ -2,6 +2,7 @@ import axios from "axios";
 const prodBaseUrl = "http://13.210.72.134:8080";
 console.log(`${prodBaseUrl}/api/config/header`);
 const api_getHeader = () => {
+  console.log(`${prodBaseUrl}/api/config/header`);
   return axios
     // .get("http://localhost:8080/api/config/header")
     .get(`${prodBaseUrl}/api/config/header`)
--- client/src/services/product/api_getAll.js
+++ client/src/services/product/api_getAll.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const prodBaseUrl = "http://13.210.72.134:8080";
 const api_getAll = () => {
+  console.log(`${prodBaseUrl}/api/product/getall`);
   return axios
     // .get("http://localhost:8080/api/product/getall")
     .get(`${prodBaseUrl}/api/product/getall`)
